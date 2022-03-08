a85b3e50d2d1169892b5e72917fbf4a5b9daf4e1
ajugoon <54783263+ajugoon@users.noreply.github.com>
2022-03-08 08:53:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+ // Create button functionality to manipulate text being displayed
+            
+        document.getElementById("button1").onclick = function() {
+
+            const element = document.getElementById("mySport");
+            element.innerHTML = "Soccer";
+
+            const info = document.getElementById("myInfo");
+            info.innerHTML = "More than 240 million people around the world play soccer regularly according to the Federation Internationale de Football Association (FIFA). The game has evolved from the sport of kicking a rudimentary animal-hide ball around into the World Cup sport it is today.vRecords trace the history of soccer back more than 2,000 years ago to ancient China. Greece, Rome, and parts of Central America also claim to have started the sport; but it was England that transitioned soccer, or what the British and many other people around the world call “football,” into the game we know today. Source: https://www.athleticscholarships.net/history-of-soccer-football.htm";
+
+        }//end onclick     
+
+        document.getElementById("button2").onclick = function() {
+
+            const element = document.getElementById("mySport");
+            element.innerHTML = "Hockey";
+
+            const info = document.getElementById("myInfo");
+            info.innerHTML = "The roots of hockey are buried deep in antiquity. Historical records show that a crude form of the game was played in Egypt 4,000 years ago and in Ethiopia around 1,000BC, whilst an ancient form of the game was also played in Iran in around 2,000BC. Various museums offer evidence that a form of the game was played by the Romans and Greeks as well as by the Aztecs several centuries before Columbus arrived in the New World. The modern game of hockey emerged in England in the mid-18th century and is largely attributed to the growth of public schools, such as Eton. The first Hockey Association was formed in the UK in 1876 and drew up the first formal set of rules. The original association survived for just six years but, in 1886, it was revived by nine founding member clubs. Source: http://www.fih.ch/hockey-basics/history/";
+
+        }//end onclick     
+
+        document.getElementById("button3").onclick = function() {
+
+            const element = document.getElementById("mySport");
+            element.innerHTML = "Baseball";
+
+            const info = document.getElementById("myInfo");
+            info.innerHTML = "Most cultures have some sort of stick and ball game, cricket being the most well-known. While the exact origins of baseball are unknown, most historians agree that it is based on the English game of rounders. It began to become quote popular in this country in the early 19th century, and many sources report the growing popularity of a game called “townball”, “base”, or “baseball”. Throughout the early part of that century, small towns formed teams, and baseball clubs were formed in larger cities. In 1845, Alexander Cartwright wanted to formalize a list of rules by which all team could play. Much of that original code is still in place today. Although popular legend says that the game was invented by Abner Doubleday, baseball’s true father was Cartwright. Source: http://www.seanlahman.com/baseball-archive/brief-history-of-baseball/";
+
+        }//end onclick       
+ 
