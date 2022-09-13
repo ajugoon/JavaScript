29df7161f45ff4255bf03ab1b314f9c4000792cc
ajugoon <54783263+ajugoon@users.noreply.github.com>
2022-09-13 11:36:42 -0400

    Create example2-circle.js

---
+++ example2-circle.js
@@ -0,0 +1,13 @@
+// This is a simple program that caluclates the area of a circle
+
+// establish the value of PI as a constant value
+const PI = 3.14159;
+
+// define some variables and give placeholder values where needed 
+let area = 0;
+let radius = 5;
+
+// the area of the circle is given by A = PI * (r ** 2)
+area = (PI * radius * radius);
+
+console.log("The Area of the Circle is: " + area + " sq. units")
