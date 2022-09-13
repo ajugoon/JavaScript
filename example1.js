bd2cfca19c03571bc915e61bf3643b67dc34ff54
ajugoon <54783263+ajugoon@users.noreply.github.com>
2022-09-13 11:35:39 -0400

    Create example1.js

---
+++ example1.js
@@ -0,0 +1,7 @@
+// This is a simple program that adds two numbers together.
+// Example 1
+// Author: Mr. Jugoon
+// Date: September 9th, 2022 
+let num1 = 1; 
+let num2 = 2;
+console.log(num1 + num2); 
