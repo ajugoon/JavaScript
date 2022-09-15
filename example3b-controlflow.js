29e99926854107fed19f379fc54ed098de9a8b70
ajugoon <54783263+ajugoon@users.noreply.github.com>
2022-09-15 11:15:00 -0400

    Update example3b-controlflow.js

--- example3b-controlflow.js
+++ example3b-controlflow.js
@@ -1,5 +1,5 @@
 /* This is a simple program that introduces how to enable programs to make 
-choices based on conditional statements. */
+choices based on conditional statements and user input. */
 
 const prompt = require('prompt-sync')({sigint: true});
 
@@ -8,8 +8,7 @@ console.log(`Hey there ${name}!`);
 
 const grade = prompt('Enter your grade as a mark from 0-100: ');
 
-// ensure that data entered is considered as a Number
-myGrade = Number(grade);
+let myGrade = Number(grade);
 
 if (myGrade >= 80) {
   console.log('A: excellent work!');
@@ -18,7 +17,7 @@ else if (myGrade >= 70) {
   console.log('B: very good work!');
 } 
 else if (myGrade >= 60) {
-  console.log('C: encouraging perfomance, but more is expected!');
+  console.log('C: encouraging performance, but more is expected!');
 } 
 else {
   console.log('You need to come in for extra help!');
