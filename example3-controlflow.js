05c3bd349c13d7f01b30858726949f817c53b079
ajugoon <54783263+ajugoon@users.noreply.github.com>
2022-09-15 10:36:22 -0400

    Create example3-controlflow.js

---
+++ example3-controlflow.js
@@ -0,0 +1,17 @@
+/* This is a simple program that introduces how to enable programs to make 
+choices based on conditional statements. */
+
+const size = 10;
+
+if (size > 100) {
+  console.log('Big');
+} 
+else if (size > 20) {
+  console.log('Medium');
+} 
+else if (size > 4) {
+  console.log('Small');
+} 
+else {
+  console.log('Tiny');
+}
