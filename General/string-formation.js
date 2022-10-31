e0f60859abde45b3b7a093e69cb5efa658e961b3
ajugoon <54783263+ajugoon@users.noreply.github.com>
2022-10-30 23:24:07 -0400

    Create string-formation.js

---
+++ General/string-formation.js
@@ -0,0 +1,6 @@
+// A simple example that shows two different methods to concatenate strings
+// i.e. string "formation"
+
+const name = prompt('What is your name? ');
+console.log(`Hey there ${name}!`); // note the use of the backtick (`) here 
+console.log("Hey there " + name + "!")
