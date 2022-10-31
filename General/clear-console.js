e10f39d9501bfd2861cf64aeb71dad0d420aa0aa
ajugoon <54783263+ajugoon@users.noreply.github.com>
2022-10-30 20:55:54 -0400

    Create clear-console.js

---
+++ General/clear-console.js
@@ -0,0 +1,12 @@
+let sum = 0;
+const n = 100
+
+// looping from i = 1 to n
+// in each iteration, i is increased by 1
+for (let i = 1; i <= n; i++) {
+    sum += i;  // sum = sum + i
+}
+
+console.log('sum:', sum);
+console.log('Notice that the console will clear in 4 seconds');
+setTimeout(() => {console.clear()}, 4000);
