91229b5d54f4f599e730b027dfd82d3b6b1111c7
ajugoon <54783263+ajugoon@users.noreply.github.com>
2022-10-30 20:54:49 -0400

    Create ready-start.js

---
+++ General/ready-start.js
@@ -0,0 +1,10 @@
+// program to start the user interaction
+// initialize the variable
+let ready = 'no';
+
+// while loop will iterate until 'yes' is the response given
+while (ready != 'yes') {
+  console.log('Welcome!!!');
+  ready = String(prompt('Do you want to start the program?'));
+}
+console.log('Ok, NOW we are ready to roll!')
