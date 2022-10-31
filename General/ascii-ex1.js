35eac0391736630cfdd1174bbd05c98f1c42d630
ajugoon <54783263+ajugoon@users.noreply.github.com>
2022-10-30 20:53:20 -0400

    Create ascii-ex1.js

---
+++ General/ascii-ex1.js
@@ -0,0 +1,21 @@
+/* ASCII Art takes some time to format correctly
+and may involve some trial and error as you sort out spacing and "escaping".
+Even though this will cause your art to seem distorted,
+when it prints it will look more acceptable.
+Note: all backticks (`) must have a \ in front of them!
+Note: all backslashes (\) must have a \ in front of them!
+*/ 
+
+const myArt = `
+ ______
+|  ____|                         | |     
+| |__  __  ____ _ _ __ ___  _ __ | | ___ 
+|  __| \\ \\/ / _\` | '_ \`_ \| '_ \\| |  \/ _ \\
+| |____ >  < (_| | | | | | | |_) | |  __/
+|______/_/\\_\\__,_|_| |_| |_| .__/|_|\\___|
+                           | |           
+                           |_| 
+   
+`;    
+
+console.log(myArt);
